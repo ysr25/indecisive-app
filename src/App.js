0c9c4112460357f121c896c0e18326ada586b85e
ysr <yongsr12345@gmail.com>
2022-02-13 21:36:16 +0800

    Add front-end design, change a few variable names

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMinus, faPlus, faAnglesDown, faAnglesUp } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMinus, faPlus, faAnglesDown, faAnglesUp)
 
 function App() {
   const [input, setInput] = useState("")
   const [options, setOptions] = useState(["Watch Movie"])
   const [chosenOption, setChosenOption] = useState("")
+  const [showDelete, setShowDelete] = useState(false)
 
   function updateInput(event) {
     setInput(event.target.value)
@@ -19,39 +25,57 @@ function App() {
     }
   }
 
-  function removeOption(index) {
+  function deleteOption(index) {
     setOptions(state => state.filter((option, i) => i !== index))
   }
 
-  function removeAllOptions() {
+  function deleteAllOptions() {
     setOptions([])
   }
 
   function chooseOption() {
-    setChosenOption(options[Math.floor(Math.random() * options.length)])
+    if (options.length === 0) {
+      setChosenOption("Please add at least one option.")
+    } else {
+      setChosenOption(options[Math.floor(Math.random() * options.length)])
+    }
+  }
+
+  function toggleDelete() {
+    setShowDelete(state => !state)
   }
 
   return (
-    <div className="App">
-      <header>
-        <h1>Indecisive App</h1>
-        <h2>Put your choices in the hands of a computer.</h2>
-      </header>
-      <div>
-        <button onClick={chooseOption}>What should I do?</button>
-        <div>{chosenOption}</div>
+    <div className="app cols">
+      <div className="col left">
+        <header>
+          <h1>Indecisive App</h1>
+          Put your choices in the hands of a computer.
+        </header>
+        <div className="choose-section">
+          <button className="choose-button" onClick={chooseOption}>What should I do?</button>
+          <div className="choose-result">{chosenOption}</div>
+        </div>
+        <button className="toggle-button" onClick={toggleDelete}>
+          <FontAwesomeIcon icon={`fa-solid fa-angles-${showDelete ? "up" : "down"}`} />
+        </button>
+        {showDelete &&
+        <div className="delete-section">
+          <button className="delete-button" onClick={deleteAllOptions}>Remove All Options</button>
+        </div>}
       </div>
-      <div>
-        <form onSubmit={addOption}>
-          <input type="text" value={input} onChange={updateInput}/>
-          <input type="submit" value="Add Option"/>
+      <div className="col">
+        <form onSubmit={addOption} className="row">
+          <input className="item" type="text" value={input} onChange={updateInput} placeholder="New option" />
+          <button className="right-button" type="submit">
+            <FontAwesomeIcon icon="fa-solid fa-plus" />
+          </button>
         </form>
-        <button onClick={removeAllOptions}>Remove All</button>
-      </div>
-      <div>
-        {options.map((option, index) => <div key={index}>
-          {option}
-          <button onClick={() => removeOption(index)}>Remove Option</button>
+        {options.map((option, index) => <div key={index} className="row">
+          <div className="item">{option}</div>
+          <button className="right-button" onClick={() => deleteOption(index)}>
+            <FontAwesomeIcon icon="fa-solid fa-minus" />
+          </button>
         </div>)}
       </div>
     </div>
